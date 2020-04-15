743a87d768ea5925a631a70e1d79e4c57885b907
ziembatomasz <ziembatomasz@gmail.com>
2020-04-15 23:48:22 +0100

    Zombie Flappy Birda game, config and Screen class

---
+++ core/src/com/ziembatomasz/zombiebird/ZBGame.java
@@ -0,0 +1,12 @@
+package com.ziembatomasz.zombiebird;
+
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+
+public class ZBGame extends Game {
+    @Override
+    public void create(){
+        Gdx.app.log("ZBGame", "created");
+    }
+}
