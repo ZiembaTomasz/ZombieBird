0a7e8fe1efcdccc2cc0886ff8ebc61c9818b39c8
ziembatomasz <ziembatomasz@gmail.com>
2020-04-16 15:44:38 +0100

    add GameRender and GameWorld class with rectangle example

--- core/src/com/ziembatomasz/zombiebird/ZBGame.java
+++ core/src/com/ziembatomasz/zombiebird/ZBGame.java
@@ -3,10 +3,12 @@ package com.ziembatomasz.zombiebird;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.ziembatomasz.screens.GameScreen;
 
 public class ZBGame extends Game {
     @Override
     public void create(){
         Gdx.app.log("ZBGame", "created");
+        setScreen(new GameScreen());
     }
 }
